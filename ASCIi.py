21be6ffbc241e4485ebd3fd0d04ade857ff611bb
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-05-12 10:34:00 +0200

    (add) def of ascii characters

--- ASCIi.py
+++ ASCIi.py
@@ -1,10 +1,12 @@
 import PIL.Image
 
+ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
+
 def main():
     x = input('Enter the path to the image fiel : \n')
 
     try:
         image = PIL.Image.open(x)
     except:
-        print(x, 'Unable to find image');
+        print(x, 'Unable to find image')
 main()
