94bc333aed8ebf072a48aeb21271d22ed81ab10a
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-05-12 10:35:28 +0200

    (add) size converter

--- ASCIi.py
+++ ASCIi.py
@@ -10,3 +10,9 @@ def main():
     except:
         print(x, 'Unable to find image')
 main()
+
+def resize(image, new_width = 100):
+    width, height = image.size
+    new_height = new_width * height / width
+    return image.resize((new_width, new_height))
+resize()
