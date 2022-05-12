f9356f75e8bfdc8bcb2d2103a61da255d989c7bc
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-05-12 10:57:24 +0200

    (update/delete) add new version pixels converter
    delete old code

--- ASCIi.py
+++ ASCIi.py
@@ -14,6 +14,10 @@ def grayify(image):
     grayscale_image = image.convert('L')
     return(grayscale_image)
 
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    characters = ''.join([ASCII_CHARS[pixel//2] for pixel in pixels])
+    return(characters)
 
 def main():
     path = input('Enter the path to the image fiel : \n')
@@ -22,33 +26,4 @@ def main():
         image = PIL.Image.open(path)
     except:
         print(path, 'Unable to find image')
-
-
-
-
-    image = resize(image);
-
-    def to_greyscale(image):
-        return image.convert("L")
-    to_greyscale()
-
-    greyscale_image = to_greyscale(image)
-
-    def pixel_to_ascii(image):
-        pixels = image.getdata()
-        ascii_str = "";
-        for pixel in pixels:
-            ascii_str += ASCII_CHARS[pixel//25];
-        return ascii_str
-    pixel_to_ascii()
-
-    ascii_str = pixel_to_ascii(greyscale_image)
-
-    img_width = greyscale_image.width
-    ascii_str_len = len(ascii_str)
-    ascii_img=""
-    for i in range(0, ascii_str_len, img_width):
-        ascii_img += ascii_str[i:i+img_width] + "\n"
-    with open("ascii_image.txt", "w") as f:
-        f.write(ascii_img);
 main()
