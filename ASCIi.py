a15563f42fe7fba1a6fd5aede2ca7e0cb739dcdd
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-05-12 10:38:10 +0200

    (add) pixel converter

--- ASCIi.py
+++ ASCIi.py
@@ -16,3 +16,15 @@ def resize(image, new_width = 100):
     new_height = new_width * height / width
     return image.resize((new_width, new_height))
 resize()
+
+def to_greyscale(image):
+    return image.convert("L")
+to_greyscale()
+
+def pixel_to_ascii(image):
+    pixels = image.getdata()
+    ascii_str = "";
+    for pixel in pixels:
+        ascii_str += ASCII_CHARS[pixel//25];
+    return ascii_str
+pixel_to_ascii()
