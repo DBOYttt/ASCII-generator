6af9c1ba7770d84bbe2dabe3f3c6955169597596
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-05-12 10:53:03 +0200

    (update) grayify to new version of code

--- ASCIi.py
+++ ASCIi.py
@@ -10,6 +10,9 @@ def resize(image, new_width = 100):
         resize_image = image.resize((new_width, new_height))
         return(resize_image)
 
+def grayify(image):
+    grayscale_image = image.convert('L')
+    return(grayscale_image)
 
 
 def main():
